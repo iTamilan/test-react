0f5afdb3c945b52bb1e6edb92b4c8ec50e81e476
Tamilarasu <tponnusamy@rdalabs.com>
2020-02-16 15:41:54 +0530

    Updates

---
+++ precache-manifest.34ac1a271b25c969de088663e6c6fbf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efc3850b52ef9cf9c153345d3588821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61cdaca24201e96f3bf0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24d370710335dae81969",
+    "url": "/static/js/2.09e314f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.09e314f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cdaca24201e96f3bf0",
+    "url": "/static/js/main.4584cb49.chunk.js"
+  },
+  {
+    "revision": "20e59aa06a7effe40b0f",
+    "url": "/static/js/runtime-main.cac9a390.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
